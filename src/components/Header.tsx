2ae9e3aefbb9b7320e64e2349881ebee0e78e19d
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2022-12-23 06:02:09 -0500

    added current location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,50 @@
 import Navigation from "./Navigation";
-import location from "../assets/location.svg";
+import locationIcon from "../assets/location.svg";
 import SearchInput from "./SearchInput";
-// interface Props {
-//   onSearch: void;
-// }
+import { useEffect, useState } from "react";
+
+type Location = {
+  lat: number;
+  lng: number;
+};
+
+type NominatimResponse = {
+  address: {
+    city: string;
+  };
+};
+
 export default function Header(props: any) {
+  const [location, setLocation] = useState<Location>({ lat: 0, lng: 0 });
+  const [city, setCity] = useState("");
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (location.lat && location.lng) {
+      // Make a request to the OpenStreetMap Nominatim API
+      fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${location.lat}&lon=${location.lng}`
+      )
+        .then((response) => response.json())
+        .then((data: NominatimResponse) => {
+          // Extract the city name from the API response
+          if (data.address && data.address.city) {
+            setCity(data.address.city);
+          } else {
+            setCity("");
+          }
+        });
+    }
+    console.log("here");
+  }, [location]);
+
   return (
     <header className="min-h-[55rem] header flex flex-col justify-end items-center font-Montserrat ">
       <Navigation />
@@ -25,8 +65,8 @@ export default function Header(props: any) {
         <div className="bg-white h-32 my-7 rounded-xl flex justify-between items-center search overflow-hidden xs:w-[90rem]">
           <div className=" justify-between items-center w-1/2 text-2xl border-r-2 border-gray-300 h-full font-Inter px-5 hidden xs:flex">
             <div className="flex items-center ">
-              <img src={location} alt="location icon" />
-              <h1 className="">Location</h1>
+              <img src={locationIcon} alt="location icon" />
+              <h1 className="">{city}</h1>
             </div>
             <h1 className="text-blue-500">Change City</h1>
           </div>
