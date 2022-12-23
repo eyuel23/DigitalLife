5dcff5d9a2789ac64cf9fd8e8af5ab96fc098bfb
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2022-12-23 03:01:49 -0500

    fixed the none working most searched cities

--- src/components/LifeQuality.tsx/MostSearched.tsx
+++ src/components/LifeQuality.tsx/MostSearched.tsx
@@ -3,18 +3,23 @@ import philly from "../../assets/philly.jpg";
 import Cities from "./Cities";
 import Newyork from "../../assets/Newyork.jpeg";
 import chicago from "../../assets/chicago.jpg";
-
+import la from "../../assets/la.webp";
+import lasvegas from "../../assets/lasvegas.jpeg";
+import london from "../../assets/london.jpeg";
+import tokyo from "../../assets/tokyo.jpeg";
+import rome from "../../assets/rome.webp";
+import paris from "../../assets/paris.jpeg";
 export default function MostSearched(props: any) {
   const most = [
     { id: 1, name: "philadelphia", image: philly },
     { id: 2, name: "New york", image: Newyork },
     { id: 3, name: "chicago", image: chicago },
-    { id: 4, name: "Paris", image: philly },
-    { id: 5, name: "Rome", image: philly },
-    { id: 6, name: "philadelphia", image: philly },
-    { id: 7, name: "philadelphia", image: philly },
-    { id: 8, name: "philadelphia", image: philly },
-    { id: 9, name: "philadelphia", image: philly },
+    { id: 4, name: "Paris", image: paris },
+    { id: 5, name: "Rome", image: rome },
+    { id: 6, name: "tokyo", image: tokyo },
+    { id: 7, name: "las vegas", image: lasvegas },
+    { id: 8, name: "los angeles", image: la },
+    { id: 9, name: "london", image: london },
   ];
   return (
     <div className="flex flex-col items-center gap-40">
